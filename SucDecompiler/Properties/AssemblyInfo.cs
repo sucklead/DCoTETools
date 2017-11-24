97bd04e2618445de07e704e1a28be60a76717680
sucklead <sucklead@gmail.com>
2017-11-24 23:19:24 +0000

    Decompiler stub

---
+++ SucDecompiler/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SucDecompiler")]
+[assembly: AssemblyDescription("http://dcotetools.sucklead.com/p/sucdecompiler.html")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sucklead")]
+[assembly: AssemblyProduct("SucDecompiler")]
+[assembly: AssemblyCopyright("Copyright © Sucklead 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5a78bef8-8e3f-4953-9cb3-f10d43a04f72")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
